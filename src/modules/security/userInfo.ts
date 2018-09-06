538e3f1b6e4befb7bbd25b88ec0e7c1825ff2f92
MyERP <tu.tran@yahoo.com>
2018-09-06 17:15:27 +0700

    add lifecycle hooks

--- src/modules/security/userInfo.ts
+++ src/modules/security/userInfo.ts
@@ -1,19 +1,34 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Component, Input, Output, SimpleChanges, OnChanges, OnInit } from "@angular/core";
 import {UserService} from "./userService";
+
+class BaseControl implements OnChanges{
+    ngOnChanges(changes: SimpleChanges): void{
+        for(let pro in changes){
+            if(!changes.hasOwnProperty(pro)){continue;}
+            let eventHandlerName ="on"+pro+"Changed";
+            if(!this[eventHandlerName]){continue;}
+            this[eventHandlerName](changes[pro].currentValue);
+        }
+    }
+}
 
 @Component({
     selector:"user-info",
     templateUrl:"src/modules/security/userInfo.html"
 })
-export class UserInfo{
-    @Input() firstName: any;
-    @Output() firstNameChange: EventEmitter<string> = new EventEmitter();
+export class UserInfo extends BaseControl implements OnInit {
+    public user: any;
+    @Input() userId: any;
+    private userService: UserService;
     constructor(userService: UserService){
-        console.log("User Info:", userService.getUsers());
+        super();
+        this.userService=userService;
     }
-
-    public onSaveClicked():void{
-        this.firstNameChange.emit(this.firstName);
+    public ngOnInit(){
+        this.user=this.userService.getUser(this.userId);
     }
+    // public onuserIdChanged(newVal:string):void{
+    //     this.user=this.userService.getUser(newVal);
+    // }
 }
-// injector = parent injector + provider
+
