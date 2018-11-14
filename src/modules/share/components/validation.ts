510106c96bfae6b27ae6ab2fe21e63f61de3388b
MyERP <tu.tran@yahoo.com>
2018-11-14 17:44:19 +0700

    add validation_failed css

--- src/modules/share/components/validation.ts
+++ src/modules/share/components/validation.ts
@@ -1,17 +1,22 @@
-import { Directive, OnInit,Input } from "@angular/core";
+import { Directive, OnInit,Input, ElementRef } from "@angular/core";
 import { IEventManager } from "../event/ieventManager";
 import { ValidationEvent } from "../event/validationEvent";
-import { IoCNames } from "../models/enum";
+import { IoCNames, ValidationStatus } from "../models/enum";
 
 @Directive({
     selector:"[validation]"
 })
 export class Validation implements OnInit{
+    private static INVALID_CLASS="validation_failed";
     private errorKeys:Array<string>=[];
     @Input("validation") set keys(keys:Array<string>){
         this.errorKeys=keys;
     }
-
+    private dom: any;
+    constructor(element: ElementRef){
+        this.dom= element.nativeElement;
+        console.log(this.dom);
+    }
     public ngOnInit(){
         if(!this.errorKeys || this.errorKeys.length<=0){return;}
         let eventManager:IEventManager = window.ioc.resolve(IoCNames.IEventManager);
@@ -23,7 +28,13 @@ export class Validation implements OnInit{
         });
     }
     private onValidationEventRaised(ev: ValidationEvent){
-        console.log(ev);
+        if(ev.status==ValidationStatus.Passed){
+            this.dom.classList.remove(Validation.INVALID_CLASS);
+
+        }
+        if(ev.status==ValidationStatus.Failed){
+            this.dom.classList.add(Validation.INVALID_CLASS);
+        }
     }
 
 }
